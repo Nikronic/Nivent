7e4fbd6ce1c3da92b268e8f708c81f241ec6a4b0
M. Doosti Lakhani <nikan.doosti@outlook.com>
2018-12-27 18:38:12 +0330

    some fix
    Took 45 minutes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var loadFile = function (event) {
-    var output = document.getElementById('output');
+var loadFile = function (event, target_id) {
+    var output = document.getElementById(target_id);
     output.src = URL.createObjectURL(event.target.files[0]);
 };
+
